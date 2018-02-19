bc2b1c5869fdb4936e92a035f89e219680641676
DerekBryant <dwbryan@nppd.com>
2018-02-19 12:16:12 -0600

    further initial commenting.

--- plot1.R
+++ plot1.R
@@ -2,3 +2,14 @@
 
 #this is just the initial file for structuring. 
 
+#Initial step will be to call in the power consumption data
+
+#Next this will be subsetted. The subsetting will exclude 
+#everything outside of the dates 2007-02-01 and 2007-02-02
+#or first and second days of Feb. 2007
+
+#this will be pushed to the other scripts. 
+
+#this particular script will then create a bar graph with red bars, etc. 
+#according to the assignment spec. 
+

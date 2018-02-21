be18787c093ef32a72dd26f663438fdaffc4e061
DerekBryant <dwbryan@nppd.com>
2018-02-21 13:00:21 -0600

    20180221 1258 I have figured out how to do the first plaot. I was having issues with the time / date formatting as when I called to the data I was only getting loac time shifted. strptime fixed this.
    I have inserted the data aquistion method into each of the scripts and have the first plot re-created.

--- plot1.R
+++ plot1.R
@@ -1,18 +1,7 @@
 #this script is intended to recreate plot 1
 
-#this is just the initial file for structuring. 
-
-#Initial step will be to call in the power consumption data
-
-#Next this will be subsetted. The subsetting will exclude 
-#everything outside of the dates 2007-02-01 and 2007-02-02
-#or first and second days of Feb. 2007
-
-#this will be pushed to the other scripts. 
-
-
-library(lubridate)
-library(sqldf)
+#library(lubridate)
+#library(sqldf)
 
 RawRead <- read.csv(file = "RawData/household_power_consumption.txt", 
          sep = ";",
@@ -26,20 +15,38 @@ RawRead <- read.csv(file = "RawData/household_power_consumption.txt",
 
 #RawRead$Time <- hms::as.hms(as.character(RawRead$Time))
 
-RawRead$TimeDate <- lubridate::dmy_hms(paste(RawRead$Date, RawRead$Time))
+# RawRead$TimeDate <- lubridate::dmy_hms(
+#   paste(
+#     RawRead$Date,
+#     RawRead$Time)
+#   )
 
+RawRead$TimeDate <- strptime(
+  paste(
+    RawRead$Date,
+    RawRead$Time),
+  format = "%d/%m/%Y %H:%M:%S"
+)
+
+#RawRead$TimeDate2 <- as.POSIXlt(RawRead$TimeDate)
+                                       
 #names(RawRead)
 
 Feb_1_2_PowData <- subset(RawRead,
-                          TimeDate >= "2007-2-1 00:00:01" & TimeDate <= "2007-2-2 23:59:59",
-                          select = c(TimeDate, 
-                                     Global_active_power, 
-                                     Global_reactive_power, 
-                                     Voltage, 
-                                     Global_intensity, 
-                                     Sub_metering_1, 
-                                     Sub_metering_2, 
-                                     Sub_metering_3)
+                          TimeDate >= as.POSIXlt("2007-2-1 00:00:00") 
+                          & TimeDate <= as.POSIXlt("2007-2-2 23:59:59")
+                          #,
+                          # select = c(TimeDate, 
+                          #            Global_active_power, 
+                          #            Global_reactive_power, 
+                          #            Voltage, 
+                          #            Global_intensity, 
+                          #            Sub_metering_1, 
+                          #            Sub_metering_2, 
+                          #            Sub_metering_3)
                           )
 
-hist(Feb_1_2_PowData$Global_active_power)
+hist(Feb_1_2_PowData$Global_active_power, 
+     xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power", 
+     col = "red")
--- plot3.R
+++ plot3.R
@@ -1,3 +1,47 @@
 #this script is intended to recreate plot 3
 
-#this is just the initial file for structuring. 
+RawRead <- read.csv(file = "RawData/household_power_consumption.txt", 
+                    sep = ";",
+                    header = TRUE,
+                    na.strings = "?")
+
+#this particular script will then create a bar graph with red bars, etc. 
+#according to the assignment spec. 
+
+#RawRead$Date <- as.Date(RawRead$Date, format = "%d/%m/%Y")
+
+#RawRead$Time <- hms::as.hms(as.character(RawRead$Time))
+
+# RawRead$TimeDate <- lubridate::dmy_hms(
+#   paste(
+#     RawRead$Date,
+#     RawRead$Time)
+#   )
+
+RawRead$TimeDate <- strptime(
+  paste(
+    RawRead$Date,
+    RawRead$Time),
+  format = "%d/%m/%Y %H:%M:%S"
+)
+
+#RawRead$TimeDate2 <- as.POSIXlt(RawRead$TimeDate)
+
+#names(RawRead)
+
+Feb_1_2_PowData <- subset(RawRead,
+                          TimeDate >= as.POSIXlt("2007-2-1 00:00:00") 
+                          & TimeDate <= as.POSIXlt("2007-2-2 23:59:59")
+                          #,
+                          # select = c(TimeDate, 
+                          #            Global_active_power, 
+                          #            Global_reactive_power, 
+                          #            Voltage, 
+                          #            Global_intensity, 
+                          #            Sub_metering_1, 
+                          #            Sub_metering_2, 
+                          #            Sub_metering_3)
+)
+
+
+##TODO: make line plot of of active sub metering (three of them) power through these two days.
